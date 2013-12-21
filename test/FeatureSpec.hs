ffd06a25914ee0f74dda98b373bb8f730e8c008b
Fujimura Daisuke <me@fujimuradaisuke.com>
2013-12-21 17:44:36 +0900

    Update submodule and specs

--- test/FeatureSpec.hs
+++ test/FeatureSpec.hs
@@ -46,79 +46,79 @@ features = do
 
   describe "LICENSE" $ do
     it "should include author" $  do
-      compiled <- readFile "LICENSE"
+      compiled <- readFile "testapp/LICENSE"
       compiled `shouldContain` "Fujimura Daisuke"
 
     it "should include year" $  do
       (year,_,_) <- (toGregorian . utctDay) <$> getCurrentTime
-      compiled   <- readFile "LICENSE"
+      compiled   <- readFile "testapp/LICENSE"
       compiled `shouldContain` show year
 
   describe "README.md" $ do
     it "should include name" $  do
-      compiled <- readFile "README.md"
+      compiled <- readFile "testapp/README.md"
       compiled `shouldContain` "testapp"
 
   describe "module-name.cabal" $ do
     it "should include name" $  do
-      compiled <- readFile "testapp.cabal"
+      compiled <- readFile "testapp/testapp.cabal"
       compiled `shouldContain` "testapp"
 
     it "should include author" $  do
-      compiled <- readFile "testapp.cabal"
+      compiled <- readFile "testapp/testapp.cabal"
       compiled `shouldContain` "Fujimura Daisuke"
 
     it "should include email" $  do
-      compiled <- readFile "testapp.cabal"
+      compiled <- readFile "testapp/testapp.cabal"
       compiled `shouldContain` "me@fujimuradaisuke.com"
 
     it "should include exposed-modules" $  do
-      compiled <- readFile "testapp.cabal"
+      compiled <- readFile "testapp/testapp.cabal"
       compiled `shouldContain` "Exposed-Modules:      System.Awesome.Library"
 
     it "should include other-modules" $  do
-      compiled <- readFile "testapp.cabal"
+      compiled <- readFile "testapp/testapp.cabal"
       compiled `shouldContain` "Other-Modules:        System.Awesome.Library.Internal"
 
   describe "directory" $ do
     it "should be made according to given module name" $  do
-      doesDirectoryExist "src/System/Awesome/Library" `shouldReturn` True
+      doesDirectoryExist "testapp/src/System/Awesome/Library" `shouldReturn` True
 
   describe "Main module" $ do
     it "should be made" $  do
-      doesFileExist "src/System/Awesome/Library.hs" `shouldReturn` True
+      doesFileExist "testapp/src/System/Awesome/Library.hs" `shouldReturn` True
 
     it "should include proper module name" $  do
-      compiled <- readFile "src/System/Awesome/Library.hs"
+      compiled <- readFile "testapp/src/System/Awesome/Library.hs"
       compiled `shouldContain` "module System.Awesome.Library"
 
   describe "Internal module" $ do
     it "should be made" $  do
-      doesFileExist "src/System/Awesome/Library/Internal.hs" `shouldReturn` True
+      doesFileExist "testapp/src/System/Awesome/Library/Internal.hs" `shouldReturn` True
 
     it "should include proper module name" $  do
-      compiled <- readFile "src/System/Awesome/Library/Internal.hs"
+      compiled <- readFile "testapp/src/System/Awesome/Library/Internal.hs"
       compiled `shouldContain` "module System.Awesome.Library.Internal"
 
   describe "Spec.hs" $ do
     it "should be made" $  do
-      doesFileExist "test/Spec.hs" `shouldReturn` True
+      doesFileExist "testapp/test/Spec.hs" `shouldReturn` True
 
     it "should include proper content" $  do
-      compiled <- readFile "test/Spec.hs"
+      compiled <- readFile "testapp/test/Spec.hs"
       compiled `shouldContain` "{-# OPTIONS_GHC -F -pgmF hspec-discover #-}"
 
   describe "Main spec" $ do
     it "should be made" $  do
-      doesFileExist "test/System/Awesome/LibrarySpec.hs" `shouldReturn` True
+      doesFileExist "testapp/test/System/Awesome/LibrarySpec.hs" `shouldReturn` True
 
     it "should include proper content" $  do
-      compiled <- readFile "test/System/Awesome/LibrarySpec.hs"
+      compiled <- readFile "testapp/test/System/Awesome/LibrarySpec.hs"
       compiled `shouldContain` "module System.Awesome.LibrarySpec (main, spec) where"
 
   describe ".gitignore" $ do
     it "should be made" $  do
-      doesFileExist ".gitignore" `shouldReturn` True
+      doesFileExist "testapp/.gitignore" `shouldReturn` True
 
 setupWithConfigurationFile :: IO () -> IO ()
 setupWithConfigurationFile action = do
