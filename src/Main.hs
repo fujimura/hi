3627429570902a5224889e21a490cba051ad3a69
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-09-08 20:01:42 +0900

    Show help text with no arguments

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,26 @@
 module Main where
 
-import           Hi                    (run)
-import           Hi.Option             (buildOption, defaultRepo)
-import           Hi.Option.Commandline (commandLineOption)
-import qualified Hi.Version            as Version
+import           Hi                           (run)
+import           Hi.Option                    (buildOption, defaultRepo)
+import           Hi.Option.Commandline        (commandLineOption)
+import qualified Hi.Version                   as Version
 
+import           Data.Monoid                  (mempty)
 import           Options.Applicative
+import           System.Environment           (getArgs)
+import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
 main :: IO ()
-main = execParser opts >>= buildOption >>= run
+main = do
+    args <- getArgs
+    if null args
+      then showHelpText (prefs idm) opts
+      else execParser opts >>= buildOption >>= run
   where
     opts = info (helper <*> (version <*> commandLineOption))
       ( fullDesc
      <> header "Generate a haskell project based on a template from github."
-     <> footer footerText)
+     <> footerDoc (Just (PP.text footerText)))
     -- TODO Newline won't rendered.
     footerText = unlines [ ""
                            , "If repository is not provided, it defaults to the repository at"
@@ -27,3 +34,6 @@ main = execParser opts >>= buildOption >>= run
       (  short 'v'
       <> long "version"
       <> help "Print version information" )
+    showHelpText :: ParserPrefs -> ParserInfo a -> IO ()
+    showHelpText pprefs pinfo = handleParseResult . Failure $
+      parserFailure pprefs pinfo ShowHelpText mempty
