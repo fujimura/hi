61addc90f91e8febb772b897786356b2b29e7657
Fujimura Daisuke <me@fujimuradaisuke.com>
2015-01-09 18:00:16 +0800

    Stop dropping all regular files

--- src/Hi.hs
+++ src/Hi.hs
@@ -1,34 +1,37 @@
-{-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns  #-}
+{-# LANGUAGE RecordWildCards #-}
 module Hi
   (
     run
   , process
   ) where
 
-import           Hi.Directory             (inDirectory)
-import           Hi.FilePath              (rewritePath)
-import qualified Hi.Git                   as Git
-import           Hi.Template              (readTemplates)
+import           Hi.Directory            (inDirectory)
+import           Hi.FilePath             (rewritePath)
+import qualified Hi.Git                  as Git
+import           Hi.Template             (readTemplates)
 import           Hi.Types
 
 import           Control.Applicative
 import           Control.Monad
-import qualified Data.ByteString          as BS (writeFile, concat)
-import qualified Data.ByteString.Lazy     as LBS (toChunks)
-import           Data.Maybe               (fromJust)
-import qualified Data.Text                as T (pack, unpack)
-import           Data.Text.Encoding       (decodeUtf8)
-import           Data.Text.Lazy.Encoding  (encodeUtf8)
-import           Data.Text.Template       (Context, substitute)
-import           System.Directory         (createDirectoryIfMissing)
-import           System.FilePath          (dropFileName, joinPath, splitPath, normalise)
-import           System.Process           (system)
+import qualified Data.ByteString.Char8   as BS
+import qualified Data.ByteString.Lazy    as LBS (toChunks)
+import           Data.List               ((\\))
+import           Data.Maybe              (fromJust)
+import qualified Data.Text               as T (pack, unpack)
+import           Data.Text.Encoding      (decodeUtf8)
+import           Data.Text.Lazy.Encoding (encodeUtf8)
+import           Data.Text.Template      (Context, substitute)
+import           System.Directory        (createDirectoryIfMissing)
+import           System.FilePath         (dropFileName, joinPath, normalise,
+                                          splitPath)
+import           System.Process          (system)
 
 -- | Run 'hi'.
 run :: Option -> IO ()
 run option@(Option {templateSource}) = do
     putStrLn $ "Creating new project with " ++ sourceName templateSource
-    writeFiles =<< showFileList =<< process option . dropExtraRegularFiles <$> readTemplates templateSource
+    writeFiles =<< showFileList =<< process option <$> readTemplates templateSource
     postProcess option
   where
     sourceName (FromRepo repository) = "git repository:" ++ Git.expandUrl repository
@@ -60,7 +63,7 @@ showFileList files = do
 --
 -- 2. substitute arguments
 process :: Option -> Files -> Files
-process Option {..} = map go
+process Option {..} files = map go (dropExtraRegularFiles files)
   where
     go (TemplateFile path content) = TemplateFile (rewritePath' path) (substitute' content)
     go (RegularFile  path content) = RegularFile  (rewritePath' path) content
@@ -83,12 +86,19 @@ postProcess Option {packageName, afterCommands} = do
     void $ inDirectory packageName $ forM_ afterCommands (void . system)
 
 -- | Drop 'RegularFile's if there is a 'TemplateFile' which has same name
+--
+-- >>> dropExtraRegularFiles [TemplateFile "foo" (BS.pack "e"), RegularFile "foo" (BS.pack "e")]
+-- [TemplateFile {getFilePath = "foo", getFileContents = "e"}]
+--
+-- >>> dropExtraRegularFiles [RegularFile "foo" (BS.pack "e")]
+-- [RegularFile {getFilePath = "foo", getFileContents = "e"}]
+--
 dropExtraRegularFiles :: Files -> Files
 dropExtraRegularFiles []     = []
 dropExtraRegularFiles xs = go (map getFilePath xs) xs
   where
     go _ []       = []
-    go paths (y@(RegularFile p _):ys)  = if p `elem` paths
+    go paths (y@(RegularFile p _):ys)  = if p `elem` (paths \\ [p])
                                            then go paths ys
                                            else y : go paths ys
     go paths (y@(TemplateFile _ _):ys) = y : go paths ys
