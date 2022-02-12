09b7983b93ecae5fdce1e3ac12f2004f562a58d5
Daisuke Fujimura <me@fujimuradaisuke.com>
2022-02-12 20:21:59 +0900

    Run doctest by recommended command in CI

--- test/doctests.hs
+++
@@ -1,6 +0,0 @@
-import           Control.Applicative
-import           System.Process
-import           Test.DocTest
-
-main :: IO ()
-main = doctest =<< lines <$> readProcess "git" ["ls-files", "src"] []
